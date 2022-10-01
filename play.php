00f2f0ed48cff35947d1c6ecba7681f782aa1067
Chemaclass <chemaclass@outlook.es>
2022-10-02 01:45:23 +0200

    start will compale and play

--- play.php
+++ play.php
@@ -2,23 +2,11 @@
 
 declare(strict_types=1);
 
-use SebastianBergmann\Timer\Timer;
-
 require_once __DIR__ . "/vendor/autoload.php";
 
-$phelMode = getenv('PHEL_MODE');
-echo "Phel mode: $phelMode\n";
-
-$timer = new Timer();
-$timer->start();
-echo 'Starting...' . PHP_EOL;
-
-if ($phelMode === 'slow') {
-    Phel\Phel::run(__DIR__, 'phel_snake\\game', 'play');
-} else {
-    $GLOBALS['argv'][] = 'play';
-    require_once __DIR__ . "/out/phel_snake/game.php";
+$compiledFile = __DIR__ . "/out/phel_snake/game.php";
+if (!file_exists($compiledFile)) {
+    exit('You have to compile the game first. Try with: `composer compile`');
 }
-
-echo 'Total time: ' . $timer->stop()->asString() . PHP_EOL;
-
+$GLOBALS['argv'][] = 'play';
+require_once $compiledFile;
