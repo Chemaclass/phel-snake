8054dfbf0f1c653609f223d31050cafa58b89025
Chemaclass <chemaclass@outlook.es>
2023-02-16 17:23:28 +0100

    Fix play.php

--- play.php
+++ play.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 require_once __DIR__ . "/vendor/autoload.php";
 
-$compiledFile = __DIR__ . "/out/phel_snake/game.php";
+$compiledFile = __DIR__ . "/out/phel_snake/main.php";
 if (!file_exists($compiledFile)) {
     echo 'Building the project...';
-    exec('vendor/bin/phel compile --no-cache');
+    exec('vendor/bin/phel build --no-cache');
 }
 
 require_once $compiledFile;
