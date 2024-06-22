abada52498e1d7e5856ff7e97127989c4b48989e
Chemaclass <chemaclass@outlook.es>
2024-06-22 07:52:38 +0200

    upgrade phel 0.15

--- phel-config.php
+++ phel-config.php
@@ -2,14 +2,14 @@
 
 declare(strict_types=1);
 
+use Phel\Config\PhelBuildConfig;
 use Phel\Config\PhelConfig;
-use Phel\Config\PhelOutConfig;
 
 return (new PhelConfig())
     ->setSrcDirs(['src'])
     ->setTestDirs(['tests'])
     ->setVendorDir('vendor')
-    ->setOut((new PhelOutConfig())
+    ->setBuildConfig((new PhelBuildConfig())
         ->setMainPhelNamespace('phel-snake\main')
         ->setMainPhpPath('out/main.php'))
     ->setIgnoreWhenBuilding(['local.phel'])
